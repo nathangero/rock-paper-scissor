67aea7bce24cd5d0af89aaeec4ad9d6c4abcc02c
nathangero <nathangero@yahoo.com>
2023-09-27 12:06:35 -0700

    implement user choosing r p or s

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 var playButton = document.querySelector("#play-button");
 
 function playGame() {
-    console.log("hello from playGame")
+    var doesUserWantToPlay = confirm("Do you want play Rock, Paper, Scissors?");
+
+    if (!doesUserWantToPlay) {
+        alert("Goodbye");
+        return;
+    }
+
+    var userChoice = prompt("Enter R, P, or S");
+    if (userChoice != "R" || userChoice != "P" || userChoice != "S") {
+        alert("Please enter R, P, or S. Please try again.");
+        return;
+    }
+}
+
+// try later? repeatedly asking the user to have the valid input.
+function userPicksRps() {
+    var userChoice = prompt("Enter R, P, or S");
+    if (userChoice != "R" || userChoice != "P" || userChoice != "S") {
+        alert("Please enter R, P, or S");
+        
+
+    }
 }
 
 playButton.addEventListener("click", playGame);
